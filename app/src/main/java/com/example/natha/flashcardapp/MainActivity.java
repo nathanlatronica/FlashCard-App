18be7045bf65e68c331dbd9c73cc0713b47eaf37
nathanlatronica <nathan.latronica@gmail.com>
2018-10-13 15:47:18 -0700

    first

---
+++ app/src/main/java/com/example/natha/flashcardapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.natha.flashcardapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.my_red, null));
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
